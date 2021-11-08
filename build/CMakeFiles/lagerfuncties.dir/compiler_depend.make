cef93e2d8a0fefe4f7cbaa150f8d25d18bd9fda6
Pepijn De Boever <93916164+pepijndeboever@users.noreply.github.com>
2021-11-08 14:19:56 +0100

    build

--- build/CMakeFiles/lagerfuncties.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for lagerfuncties.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/test/lagerberekeningprogramma
-# Build directory: /home/test/lagerberekeningprogramma/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
